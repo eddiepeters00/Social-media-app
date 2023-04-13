5ffcb07a3e5c51bcfe65052e265992ef82f747a4
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-04-13 10:08:02 +0200

    small upd

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,11 @@ const signInForm = document.querySelector('form') as HTMLFormElement;
 signInForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    console.log('clicked btn')
-
     document.querySelectorAll('input').forEach(input => {
-        console.log('Kommer hit')
-
         if (input.value !== null && input.value !== '') {
             userObj[input.name] = input.value;
         }
-
     });
-
-        console.log('innan login handler')
         logInHandler(userObj);
 });
 
@@ -29,17 +22,13 @@ signInForm.addEventListener('submit', e => {
 //Check if user exists in db
 async function logInHandler(userObj: UserInput) {
     console.log(userObj);
-    console.log('Handle log in');
-
     const foundUser: Object = await findUserInDb(userObj);
     if (!isEmpty(foundUser)) {
-        //Prompt message to user
         console.log('Error');
     } else {
-        console.log('Logging in');
+        console.log('Logging in...');
         localStorage.setItem('user', JSON.stringify(Object.values(foundUser)));
 
-        //Add animation? 
         setTimeout(() => {
             location.assign("./html/profile.html");
         }, 1000);
