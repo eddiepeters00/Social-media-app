3b1b953fff6caa8510c0ec0f9c1704bef6bdc6aa
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-04-14 16:32:12 +0200

    nu

--- docs/index.c2434033.js
+++
@@ -1,2 +0,0 @@
-console.log("Index.ts is running");
-//# sourceMappingURL=index.c2434033.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
 console.log('Index.ts is running');
+
+import { findUserInDb } from "../src/modules/firebase";
+import { UserInput, isEmpty } from "../src/modules/interfaces";
+
+
+const userObj: UserInput = {};
+console.log('Start');
+
+//Get userinputs from register-form
+const signInForm = document.querySelector('form') as HTMLFormElement;
+signInForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    document.querySelectorAll('input').forEach(input => {
+        if (input.value !== null && input.value !== '') {
+            userObj[input.name] = input.value;
+        }
+    });
+        logInHandler(userObj);
+});
+
+
+//Check if user exists in db
+async function logInHandler(userObj: UserInput) {
+    console.log(userObj);
+    const foundUser: Object = await findUserInDb(userObj);
+    if (!isEmpty(foundUser)) {
+        console.log('Error');
+    } else {
+        console.log('Logging in...');
+        localStorage.setItem('user', JSON.stringify(Object.values(foundUser)));
+        location.replace("/FE22-js2-slutprojekt-eddie-peters/html/profile.html");
+    }
+}
