4fd3cee2e9c40721b4b467424adac9c7d893317d
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-04-18 09:50:50 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,6 @@ async function logInHandler(userObj: UserInput) {
     } else {
         console.log('Logging in...');
         localStorage.setItem('user', JSON.stringify(Object.values(foundUser)));
-        location.assign("/html/profile.html");
+        location.assign("/FE22-js2-slutprojekt-eddie-peters/html/profile.html");
     }
 }
